39b34f2ae951fdc654c7c16b0decdfcd8aa3bc34
mcindoe <conormcindoe1@gmail.com>
2019-09-11 14:12:27 +0100

    basic classes initial commit

---
+++ Card.py
@@ -0,0 +1,34 @@
+suits = ['club', 'diamond', 'heart', 'spade']
+
+class Card():
+    def __init__(self, rank, suit):
+        assert rank in range(2, 15)
+
+        # allow construction by first letter of suit
+        if len(suit) == 1:
+            assert suit in [x[0] for x in suits]
+            suit = suits[[x[0] for x in suits].index(suit)]
+
+        assert suit in suits
+        self.suit = suit
+        self.rank = rank
+
+    @staticmethod
+    def from_value(val):
+        '''Maps a value in [0, 51] onto a Card instance'''
+
+        suit = suits[int(val/13)]
+        rank = val % 13 + 2
+        return Card(rank, suit)
+
+    def __repr__(self):
+        return f'Card({self.rank}, {self.suit})'
+
+    def __str__(self):
+        if self.rank >= 10:
+            card_name = ['T', 'J', 'Q', 'K', 'A'][self.rank - 10]
+        else:
+            card_name = self.rank
+
+        return f'{card_name}{self.suit[0]}'
+
---
+++ Deck.py
@@ -0,0 +1,26 @@
+import json
+import random
+
+from Card import Card
+
+class Deck():
+    def __init__(self):
+        self.cards = [Card.from_value(n) for n in range(52)]
+        self.discards = []
+
+    def __str__(self):
+        return json.dumps([str(card) for card in self.cards], indent = 2)
+
+    def __len__(self):
+        return len(self.cards)
+
+    def shuffle(self):
+        self.cards = self.cards + self.discards
+        self.discards = []
+        random.shuffle(self.cards)
+
+    def next(self):
+        card = self.cards.pop(0)
+        self.discards.append(card)
+        return card
+
